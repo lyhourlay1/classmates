dbbd8b825d747b1ef6220b42541f631e1c07cca2
Lyhour Lay <lyhourlay1@gmail.com>
2021-09-21 15:35:55 -0700

    Start Chess

---
+++ chess/board.rb
@@ -0,0 +1,31 @@
+class Board
+    def initialize
+        @rows = Array.new(8) {Array.new(8)}
+        @null_piece = Piece.new
+    end
+    def [](pos)
+    end
+    def []=(pos)
+    end
+    def move_piece(color, start_pos, end_pos)
+    end
+
+    def valid_pos?(pos)
+    end
+    def add_piece(piece, pos)
+    end
+    def checkmate?(color)
+    end
+    def in_check?(color)
+    end
+    def find_king(color)
+    end
+
+    def pieces
+    end
+    def dup
+    end
+    def move_piece!(color, start_pos, end_pos)
+    end
+
+end
