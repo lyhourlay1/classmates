2e13ecd8445d98dc2ccb2eea856b6b6c56eafdb9
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-09-21 14:52:03 -0700

    debugging bonus multiplier method

--- employee.rb
+++ employee.rb
@@ -1,4 +1,5 @@
 class Employee
+  attr_reader :salary
     def initialize(name, title, salary, boss)
         @name=name
         @title=title
@@ -13,14 +14,30 @@ end
 
 class Manager < Employee
     #subordinates must be an array 
-    def initialize(subordinates)
+    def initialize(name, title, salary, boss, subordinates)
+        super(name, title, salary, boss)
         @subordinates=subordinates
     end
     def bonus(multiplier)
-        total_salary= @subordinates.inject {|acc, n| acc+n}
-        bonus =total_salary*multiplier
+        total_salary = 0
+        @subordinates.each do |sub|
+          if sub.is_a?(Manager)
+            total_salary += sub.bonus(multiplier)
+          else
+            total_salary += sub.salary * multiplier
+          end
+        end
+        total_salary
+        # total = total_salary.inject { |acc, n| acc + n }
+        # bonus = total * multiplier
     end
 end
 
-darren= Manager.new('Darren', 'TA Manager', 78000, ned,[david, shawna])
+shawna = Employee.new('Shawna', 'TA', 12000, 'Darren')
+david = Employee.new('David', 'TA', 10000, 'Darren')
+darren= Manager.new('Darren', 'TA Manager', 78000, 'Ned',[david, shawna])
 ned= Manager.new('Ned', 'Founder', 1000000, nil, [darren])
+
+p ned.bonus(5) #500,000
+# p darren.bonus(4) #88,000
+# p david.bonus(3) #30,000
