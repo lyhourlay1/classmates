12d32e594dbc85162d0e45a897343f47a073a7a8
Lyhour Lay <lyhourlay1@gmail.com>
2021-09-21 16:43:05 -0700

    Create Board

--- chess/board.rb
+++ chess/board.rb
@@ -1,17 +1,35 @@
+require_relative 'piece'
 class Board
     def initialize
         @rows = Array.new(8) {Array.new(8)}
         @null_piece = nil
+        populate
     end
 
     def populate
+        rooks= [[0,0],[0,7],[7,0],[7,7]]
+        kings =[[0,3],[7,3]]
+        queens=[[0,4],[7,4]]
+        knights=[[0,1], [0,6], [7,1],[7.6]]
+        bishops=[[0,2],[0,5],[7,2],[7,5]]
+
         @rows.each_with_index do |row, i|
             row.each_with_index do |square, j|
                 position = [i, j]
-                if position == [0, 0] || position == [0, 7] || position == [7, 0] || position == [7, 7]
-                    add_piece(Piece.new("Rook"), position)
-                elsif position == [0, 3] || position == [7, 3]
-
+                if i==6 || i==1
+                    self[position]=(Piece.new("Pawn"))
+                elsif i>=2 && i<=5
+                     self[position]=(nil)
+                elsif rooks.include?(position)
+                    self[position]=(Piece.new("Rook"))
+                elsif knights.include?(position)
+                    self[position]=(Piece.new("Knight"))
+                elsif bishops.include?(position)
+                    self[position]=(Piece.new("Bishop"))
+                elsif kings.include?(position)
+                    self[position]=(Piece.new("King"))
+                elsif queens.include?(position)
+                    self[position]=(Piece.new("Queen"))
                 end
             end
         end
@@ -25,8 +43,10 @@ class Board
         @rows[pos[0]][pos[1]] = value
     end
 
-    def move_piece(color, start_pos, end_pos)
-
+    def move_piece( start_pos, end_pos)
+        moved_piece=@rows[start_pos[0]][start_pos[1]]
+        self[start_pos[0],start_pos[1]]= @rows[end_pos[0]][end_pos[1]]
+        self[end_pos[0],end_pos[1]]= moved_piece
     end
 
     def valid_pos?(pos)
@@ -60,5 +80,4 @@ class Board
     def move_piece!(color, start_pos, end_pos)
 
     end
-
 end
